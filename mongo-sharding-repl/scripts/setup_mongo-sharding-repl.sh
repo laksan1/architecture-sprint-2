691c53d2cc2cee2915f500b2a91f18e1e6b88a77
laksan <laksan@mail.ru>
2025-02-19 09:17:57 +0300

    fix  Инициализация 2 шарда с именем shard2-replica-set

--- mongo-sharding-repl/scripts/setup_mongo-sharding-repl.sh
+++ mongo-sharding-repl/scripts/setup_mongo-sharding-repl.sh
@@ -41,9 +41,9 @@ docker compose exec -T shard2_1 mongosh --port 27021 --quiet <<EOF
 rs.initiate({
     _id : "shard2-replica-set",
     members: [
-        { _id : 0, host : "shard1_1:27021" },
-        { _id : 1, host : "shard1_2:27022" },
-        { _id : 2, host : "shard1_3:27023" }
+        { _id : 0, host : "shard2_1:27021" },
+        { _id : 1, host : "shard2_2:27022" },
+        { _id : 2, host : "shard2_3:27023" }
     ]
 });
 EOF
